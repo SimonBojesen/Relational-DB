553617980afe76d7c770b36e6b012f81525aa63a
klh <klh@minuba.dk>
2021-03-09 21:47:11 +0100

    -Added insert

--- VetDB-Mini/src/main/java/Main.java
+++ VetDB-Mini/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import Entity.Pet;
 
-import java.sql.*;
 import java.util.List;
 import java.util.Properties;
 
@@ -8,6 +7,9 @@ public class Main {
 
     public static void main(String[] args)throws Exception {
         RetrieveFromDB db = new RetrieveFromDB();
+        //change the parameters of the methods to insert a new dog/cat, and run getPets() to see all pets
+        db.insertCat("testCat1",5,7,"12345678");
+        db.insertDog("testDog1",5,"b4","12345678");
         List<Pet> pets = db.getPets();
         for (Pet pet : pets) {
             System.out.println(pet.getClass());
--- VetDB-Mini/src/main/java/RetrieveFromDB.java
+++ VetDB-Mini/src/main/java/RetrieveFromDB.java
@@ -32,15 +32,13 @@ public class RetrieveFromDB {
                     String vet_cvr = result.getString(4);
                     int lifeCount = result.getInt(5);
                     String barkpitch = result.getString(6);
-                    if(lifeCount != 0){
+                    if (lifeCount != 0) {
                         Cat cat = new Cat(id, name, age, vet_cvr, lifeCount);
                         pets.add(cat);
-                    }
-                    else if(barkpitch != null ){
+                    } else if (barkpitch != null) {
                         Dog dog = new Dog(id, name, age, vet_cvr, barkpitch);
                         pets.add(dog);
-                    }
-                    else {
+                    } else {
                         Pet pet = new Pet(id, name, age, vet_cvr);
                         pets.add(pet);
                     }
@@ -49,4 +47,21 @@ public class RetrieveFromDB {
         }
         return pets;
     }
+
+    public void insertCat(String name, int age, int life_count, String vet_cvr) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, props)) {
+            String sql = String.format("call create_cat('%s', %s, %s, '%s');", name, age, life_count, vet_cvr);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sql);
+        }
+    }
+
+    public void insertDog(String name, int age, String barkpitch, String vet_cvr) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, props)) {
+            String sql = String.format("call create_dog('%s', %s, '%s', '%s');", name, age, barkpitch, vet_cvr);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sql);
+        }
+    }
+
 }
